e210c496473a887e5307035acebb648dc97b253d
Carlos Accyole <carlos_accyole@hotmail.com>
2021-06-08 14:54:00 -0300

    Adicionando DB::beginTransaction(); no método criarSerie()

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -3,21 +3,43 @@
 namespace App\Services;
 
 use App\Serie;
+use App\Temporada;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
     public function criarSerie(string $nomeSerie, int $qtdTemporadas, int $epPorTemporada): Serie
     {
+        DB::beginTransaction();
         $serie = Serie::create(['nome' => $nomeSerie]);
+        $this->criaTemporadas($qtdTemporadas, $epPorTemporada, $serie);
+        DB::commit();
 
+        return $serie;
+    }
+
+    /**
+     * @param int $qtdTemporadas
+     * @param int $epPorTemporada
+     * @param Serie $serie
+     */
+    private function criaTemporadas(int $qtdTemporadas, int $epPorTemporada, Serie $serie): void
+    {
         for ($i = 1; $i <= $qtdTemporadas; $i++) {
             $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-            for ($j = 1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
+            $this->criaEpisodios($epPorTemporada, $temporada);
         }
+    }
 
-        return $serie;
+    /**
+     * @param int $epPorTemporada
+     * @param Temporada $temporada
+     */
+    private function criaEpisodios(int $epPorTemporada, Temporada $temporada): void
+    {
+        for ($j = 1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
