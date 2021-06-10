2d9a1282d67507e55c3f0349434dc5547d004917
Carlos Accyole <carlos_accyole@hotmail.com>
2021-06-10 11:47:48 -0300

    Adicionando episódios assistidos e mostrando para o usuário

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Episodio;
 use App\Temporada;
+use Illuminate\Http\Request;
 
 class EpisodiosController extends Controller
 {
-    public function index(Temporada $temporada)
+    public function index(Temporada $temporada, Request $request)
     {
-        $episodios = $temporada->episodios;
+        return view('episodios.index', [
+            'episodios' => $temporada->episodios,
+            'temporadaId' => $temporada->id,
+            'mensagem' => $request->session()->get('mensagem')
+        ]);
+    }
 
-        return view('episodios.index', compact('episodios'));
+    public function assistir(Temporada $temporada, Request $request)
+    {
+        $episodiosAsssistidos = $request->episodios;
+        $temporada->episodios->each(function (Episodio $episodio) use ($episodiosAsssistidos) {
+            $episodio->assistido = in_array(
+                $episodio->id,
+                $episodiosAsssistidos
+            );
+        });
+        $temporada->push();
+        $request->session()->flash('mensagem', 'Episódios marcados como assistidos');
+
+        return redirect()->back();
     }
 }
