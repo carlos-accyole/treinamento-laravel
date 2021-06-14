672393750e0a463dd58d5af5b3c2ac0457d5286e
Carlos Accyole <carlos_accyole@hotmail.com>
2021-06-14 16:57:18 -0300

    Testando a inserção

---
+++ tests/Unit/CriadorDeSerieTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Serie;
+use App\Services\CriadorDeSerie;
+use Tests\TestCase;
+
+class CriadorDeSerieTest extends TestCase
+{
+    public function testCriarSerie()
+    {
+        $criadorDeSeries = new CriadorDeSerie();
+        $nomeSerie = 'Nome de teste';
+        $serieCriada = $criadorDeSeries->criarSerie($nomeSerie, 1,1);
+
+        $this->assertInstanceOf(Serie::class, $serieCriada);
+        $this->assertDatabaseHas('series', ['nome' => $nomeSerie]);
+        $this->assertDatabaseHas('temporadas', ['serie_id' => $serieCriada->id, 'numero' => 1]);
+        $this->assertDatabaseHas('episodios', ['numero' => 1] );
+    }
+}
